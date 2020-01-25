6dceaa16ada4faa627102f0916f39c4562aeda94
PavelRyabykh <psanr@ya.ru>
2020-01-25 17:04:40 +0300

    Result of the 10th lesson

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -5,15 +5,24 @@ namespace app\controllers;
 
 
 use vendor\core\base\Controller;
+use vendor\core\Registry;
 
 class AppController extends Controller
 {
     protected $meta = [];
+    protected $app;
+
+    public function __construct($route)
+    {
+        parent::__construct($route);
+        $this->app = Registry::getInstance();
+    }
 
     public function setMeta($title = '', $description = '', $keywords = '')
     {
         $this->meta['title'] = $title;
         $this->meta['description'] = $description;
         $this->meta['keywords'] = $keywords;
+
     }
 }
